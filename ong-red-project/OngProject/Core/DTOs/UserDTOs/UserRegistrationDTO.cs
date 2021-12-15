6efcb85c45f8316098c7f601f542a518be9c21ed
DanielBDev <danielbejar.dev@gmail.com>
2021-12-15 17:29:21 -0300

    UserRegistrationDTO

---
+++ ong-red-project/OngProject/Core/DTOs/UserDTOs/UserRegistrationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OngProject.Core.DTOs.UserDTOs
+{
+    public class UserRegistrationDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
