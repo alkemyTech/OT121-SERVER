3ba962456383c6a2c9ff8e448c6d8ec7a24f6443
DanielBDev <danielbejar.dev@gmail.com>
2021-12-15 17:58:40 -0300

    Validations-DataAnnotations

--- ong-red-project/OngProject/Core/DTOs/UserDTOs/UserRegistrationDTO.cs
+++ ong-red-project/OngProject/Core/DTOs/UserDTOs/UserRegistrationDTO.cs
@@ -1,10 +1,24 @@
-﻿namespace OngProject.Core.DTOs.UserDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.DTOs.UserDTOs
 {
     public class UserRegistrationDTO
     {
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(255, ErrorMessage = "El campo {0} debe tener un maximo de {1} caracteres.")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(255, ErrorMessage = "El campo {0} debe tener un maximo de {1} caracteres.")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(320, ErrorMessage = "El campo {0} debe tener un maximo de {1} caracteres.")]
+        [EmailAddress(ErrorMessage = "El campo {0} no es un formato válido.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(64, ErrorMessage = "El campo {0} debe tener un maximo de {1} caracteres.")]
         public string Password { get; set; }
     }
 }
