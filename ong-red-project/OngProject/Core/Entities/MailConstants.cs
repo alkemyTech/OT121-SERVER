50f943c7463739a76817f81b33528219831a1600
Dario Ramirez <dframirez80@gmail.com>
2021-12-17 01:41:11 -0300

    add model, configuration and service of mail constants

---
+++ ong-red-project/OngProject/Core/Entities/MailConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Entities
+{
+    public class MailConstants
+    {
+		public string PathTemplate { get; set; }
+		public string ReplaceMailTitle { get; set; }
+		public string ReplaceMailBody { get; set; }
+		public string ReplaceMailContact { get; set; }
+		public string ReplaceMailConfirm { get; set; }
+		public string TitleMailConfirm { get; set; }
+		public string WelcomeMailBody { get; set; }
+		public string MailONG { get; set; }
+	}
+}
