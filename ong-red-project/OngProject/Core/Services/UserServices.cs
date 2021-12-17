1ed00cdc563d94a62a906859be2a373bd94eadcf
DanielBDev <danielbejar.dev@gmail.com>
2021-12-17 00:09:06 -0300

    US-21# Metodo de generacion del JWT al momento de efectuar el login

--- ong-red-project/OngProject/Core/Services/UserServices.cs
+++ ong-red-project/OngProject/Core/Services/UserServices.cs
@@ -1,9 +1,12 @@
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
+using Microsoft.IdentityModel.Tokens;
 using OngProject.Common;
 using OngProject.Core.DTOs;
 using OngProject.Core.DTOs.UserDTOs;
 using OngProject.Core.Entities;
+using OngProject.Core.Helper.Common;
 using OngProject.Core.Interfaces.IServices;
 using OngProject.Core.Interfaces.IServices.AWS;
 using OngProject.Core.Mapper;
@@ -14,6 +17,7 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace OngProject.Core.Services
@@ -26,13 +30,19 @@ namespace OngProject.Core.Services
         private IConfiguration _configuration;
         private readonly IImageService _imageServices;
         private readonly EntityMapper _mapper;
+        private readonly JWTSettings _jWTSettings;
 
-        public UserServices(IUnitOfWork _unitOfWork, IConfiguration configuration, IImageService imageServices)
+        public UserServices
+            (IUnitOfWork _unitOfWork,
+            IConfiguration configuration,
+            IImageService imageServices,
+            IOptions<JWTSettings> jWTSettings)
         {
             this._unitOfWork = _unitOfWork;
             _configuration = configuration;
             _imageServices = imageServices;
             _mapper = new EntityMapper();
+            _jWTSettings = jWTSettings.Value;
         }
 
         #endregion Object and Constructor
@@ -73,9 +83,57 @@ namespace OngProject.Core.Services
                 throw new Exception("Usuario o contraseña incorrectos.");
             }
 
-            return _mapper.FromUserToUserLoginResponseDto(userLogin);
+            JwtSecurityToken jwtSecurityToken = GenerateJWTToken(userLogin);
+
+            UserLoginResponseDTO response = new()
+            {
+                Id = userLogin.Id,
+                FirstName = userLogin.FirstName,
+                LastName = userLogin.LastName,
+                Email = userLogin.Email,
+                Photo = userLogin.Photo,
+                Role = userLogin.Role.Name,
+                CreatedAt = userLogin.CreatedAt.ToString("dd/MM/yyyy H:mm"),
+                IsDeleted = userLogin.IsDeleted,
+                JWToken = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+            };
+
+            return response;
         }
 
         #endregion Methods
+
+        #region Private Methods (Token)
+
+        private JwtSecurityToken GenerateJWTToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                new Claim("FirstName", user.FirstName),
+                new Claim("LastName", user.LastName),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("Photo", user.Photo),
+                new Claim("Role", user.Role.Name),
+                new Claim("CreatedAt", user.CreatedAt.ToString("dd/MM/yyyy H:mm")),
+                new Claim("IsDeleted", user.IsDeleted.ToString()),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jWTSettings.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jWTSettings.Issuer,
+                audience: _jWTSettings.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddDays(_jWTSettings.DurationInDays),
+                signingCredentials: signingCredentials
+                );
+
+            return jwtSecurityToken;
+        }
+
+        #endregion Private Methods (Token)
     }
 }
