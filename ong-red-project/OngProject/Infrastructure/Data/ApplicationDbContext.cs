a0bd0625e48af48da04e60518c3edea214f9a506
khphilippe <kevensherbyson@gmail.com>
2021-12-19 18:53:27 -0300

    Se agrego seed Testimonios

--- ong-red-project/OngProject/Infrastructure/Data/ApplicationDbContext.cs
+++ ong-red-project/OngProject/Infrastructure/Data/ApplicationDbContext.cs
@@ -22,6 +22,7 @@ namespace OngProject.Infrastructure.Data
             SeedContacts(builder);
             SeedOrganizations(builder);
             SeedUsers(builder);
+            SeedTestimonios(builder);
             builder.Seed();
         }
 
@@ -124,5 +125,46 @@ namespace OngProject.Infrastructure.Data
                     }                    
                 );
         }
+
+
+        private void SeedTestimonios(ModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.Entity<Testimonials>().HasData(
+                new Testimonials
+                {
+                    Id = 1,
+                    Name = "Ayuda a los niños sin hogar en santa fe",
+                    Image = "",
+                    Content = "El ONG ayudo a muchos niños en una ciudad humilde de santa fe"
+                },
+                 new Testimonials
+                 {
+                     Id = 2,
+                     Name = "Ayuda a las personas con VIH Mar del plata",
+                     Image = "",
+                     Content = "El ONG ayudo a muchas personas con VIH sin exception"
+                 },
+                  new Testimonials
+                  {
+                      Id = 3,
+                      Name = "Ayuda a las mujeres embarazadas",
+                      Image = "",
+                      Content = "El ONG ayudo a muchos mujeres "
+                  },
+                   new Testimonials
+                   {
+                       Id = 4,
+                       Name = "Ayuda a muvha gente con discapacidad en Once",
+                       Image = "",
+                       Content = "Durante la cuarentena,El ONG ayudo a mucha gente con discapacidad "
+                   });
+
+
+
+        }
+
+
     }
 }
