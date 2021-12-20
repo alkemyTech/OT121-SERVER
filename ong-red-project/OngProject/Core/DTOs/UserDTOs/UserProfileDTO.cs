9ba92de1d70c6fdfcd48e02d1f218f7769a7ae8a
Cristopher <cristopher.emodesto@gmail.com>
2021-12-20 11:53:46 -0600

    Added UserProfileDto

---
+++ ong-red-project/OngProject/Core/DTOs/UserDTOs/UserProfileDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.DTOs.UserDTOs
+{
+    public class UserProfileDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Photo { get; set; }
+        public string Role { get; set; }
+    }
+}
