bc849ca9935bacf6617a3f74c9fb59596854a6fa
DanielBDev <danielbejar.dev@gmail.com>
2021-12-23 16:41:33 -0300

    US-50# Servicio para listar los miembros

--- ong-red-project/OngProject/Core/Services/MemberServices.cs
+++ ong-red-project/OngProject/Core/Services/MemberServices.cs
@@ -14,18 +14,31 @@ using System.Threading.Tasks;
 
 namespace OngProject.Core.Services
 {
-    public class MemberServices : IMemberServices 
+    public class MemberServices : IMemberServices
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IImageService _ImageService;
         private readonly IUriService _uriService;
+        private readonly EntityMapper _mapper;
+
         public MemberServices(IUnitOfWork unitOfWork, IImageService ImageService, IUriService uriService)
         {
             _unitOfWork = unitOfWork;
             _ImageService = ImageService;
             _uriService = uriService;
+            _mapper = new EntityMapper();
         }
 
-       
+        public async Task<List<MembersDTO>> GetAllAsync()
+        {
+            var members = await _unitOfWork.MemberRepository.GetAll();
+
+            if (members == null)
+                throw new Exception("No se encontraron datos.");
+
+            var membersDto = members.Select(m => _mapper.FromMembersToMembersDto(m)).ToList();
+
+            return membersDto;
+        }
     }
 }
