6f4f0cb83ce9134f99ba72020c2ef654002fbbbd
Cristopher <cristopher.emodesto@gmail.com>
2021-12-22 21:37:39 -0600

    Added DTO

---
+++ ong-red-project/OngProject/Core/DTOs/CommentsDTOs/CommentUpdateDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.DTOs.CommentsDTOs
+{
+    public class CommentUpdateDTO
+    {
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Column(TypeName = "TEXT")]
+        [MaxLength(4000)]
+        public string Body { get; set; }
+
+    }
+}
