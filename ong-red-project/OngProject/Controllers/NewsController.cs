2f387597ce4641d2b8797f82b4d5584124a5c3ae
Cristopher <cristopher.emodesto@gmail.com>
2021-12-24 09:38:34 -0600

    Added Controller to GetAllCommentsByNews

--- ong-red-project/OngProject/Controllers/NewsController.cs
+++ ong-red-project/OngProject/Controllers/NewsController.cs
@@ -18,6 +18,26 @@ namespace OngProject.Controllers
         {
             _newsServices = newsServices;
         }
-        
+
+
+        #region Documentacion
+        /// <summary>
+        /// Endpoint para listar los comentarios pertenecientes a un post. 
+        /// </summary>
+        /// <response code="200">Listado de todos los comentarios pertenecientes a un post .</response>
+        /// <response code="401">Credenciales inválidas.</response> 
+        /// <response code="404">No se ha encontrado el dato proporcionado.</response>
+        #endregion
+        [Authorize]
+        [HttpGet("{id}/comments")]
+        public async Task<ActionResult> GetAllCommentsByNews(int id)
+        {
+            var result = await _newsServices.GetAllCommentsByNews(id);
+            if (result == null)
+                return NotFound("No existe el post");
+            return Ok(result);
+        }
+
+
     }
 }
