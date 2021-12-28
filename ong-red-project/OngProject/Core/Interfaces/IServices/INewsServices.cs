938de73ed4a51ef2f9537e2ddeb1d11580bb43a2
Dario Ramirez <dframirez80@gmail.com>
2021-12-28 17:56:58 -0300

    Add method in NewsServices

--- ong-red-project/OngProject/Core/Interfaces/IServices/INewsServices.cs
+++ ong-red-project/OngProject/Core/Interfaces/IServices/INewsServices.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using OngProject.Common;
 using OngProject.Core.DTOs;
+using OngProject.Core.DTOs.NewsDTOs;
 using OngProject.Core.Entities;
 using OngProject.Core.Helper.Pagination;
 using System;
@@ -14,5 +15,6 @@ namespace OngProject.Core.Interfaces.IServices
         Task<Comments[]> GetAllCommentsByNews(int id);
         Task<News> GetAllDataByIdAsync(int id);
         Task<News> CreateAsync(NewsDTO newsDto);
+        Task<News> UpdatePutAsync(NewsUpdateDTO newsUpdateDto);
     }
 }
