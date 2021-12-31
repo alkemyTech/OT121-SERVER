48fcb65c6c98b40455ec0e7fad297c79bd57010d
DanielBDev <danielbejar.dev@gmail.com>
2021-12-31 18:59:15 -0300

    US-52# Endpoint de actualizacion de miembros

--- ong-red-project/OngProject/Controllers/MemberController.cs
+++ ong-red-project/OngProject/Controllers/MemberController.cs
@@ -48,5 +48,69 @@ namespace OngProject.Controllers
                 return NotFound(e.Message);
             }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #region Documentation
+
+        /// <summary>
+        /// Endpoint para actualizar un miembro.
+        /// </summary>
+        /// <response code="200">Tarea ejecutada con exito devuelve un mensaje satisfactorio.</response>
+        /// <response code="400">Errores de validacion o excepciones.</response>
+
+        #endregion Documentation
+
+        //[Authorize]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromForm] MemberUpdateDTO memberUpdate, int id)
+        {
+            if (id != memberUpdate.Id)
+            {
+                return BadRequest(new Result().Fail("Los ids deben coincidir."));
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    return Ok(await _memberServices.UpdateAsync(memberUpdate));
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new Result().Fail($"Ocurrio un error al momento de intentar actulizar los datos - {ex.Message}"));
+                }
+            }
+
+            return BadRequest(new Result().Fail("Algo salio mal."));
+        }
     }
 }
