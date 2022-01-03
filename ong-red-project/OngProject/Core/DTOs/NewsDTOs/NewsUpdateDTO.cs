075723311acd64f305fc414c23bd001b1fc584ba
Dario Ramirez <dframirez80@gmail.com>
2022-01-03 04:16:09 -0300

    Change NewsUpdateDTO

--- ong-red-project/OngProject/Core/DTOs/NewsDTOs/NewsUpdateDTO.cs
+++ ong-red-project/OngProject/Core/DTOs/NewsDTOs/NewsUpdateDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -15,7 +16,7 @@ namespace OngProject.Core.DTOs.NewsDTOs
         [Required]
         public string Content { get; set; }
         [Required]
-        public string Image { get; set; }
+        public IFormFile Image { get; set; }
         [Required]
         public int CategoryId { get; set; }
     }
