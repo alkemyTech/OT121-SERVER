4b8d54a78d0b09483efddaf7c38a7d670de1dd28
Dario Ramirez <dframirez80@gmail.com>
2022-01-03 03:44:46 -0300

    Add newsCreateDto

---
+++ ong-red-project/OngProject/Core/DTOs/NewsDTOs/NewsCreateDTO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.DTOs.NewsDTOs
+{
+    public class NewsCreateDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public string Content { get; set; }
+        
+        [Required]
+        public IFormFile Image { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
