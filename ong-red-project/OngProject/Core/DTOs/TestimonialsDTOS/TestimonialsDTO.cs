23fcd6b471d288171801f87a6f162679517481ca
Dario Ramirez <dframirez80@gmail.com>
2022-01-05 21:08:30 -0300

    Add dto testimonials

---
+++ ong-red-project/OngProject/Core/DTOs/TestimonialsDTOS/TestimonialsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.DTOs.TestimonialsDTOs
+{
+    public class TestimonialsDTO
+    {
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public string Content { get; set; }
+    }
+}
