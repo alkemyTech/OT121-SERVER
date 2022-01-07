75bc516910a35320aa9746564800c7ebfdd33936
Cristopher <cristopher.emodesto@gmail.com>
2022-01-07 10:53:09 -0600

    Added documentation with Class ResultValue

--- ong-red-project/OngProject/Controllers/MemberController.cs
+++ ong-red-project/OngProject/Controllers/MemberController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using OngProject.Common;
 using OngProject.Core.DTOs;
 using OngProject.Core.Helper.Pagination;
 using OngProject.Core.Interfaces.IServices;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,45 +29,54 @@ namespace OngProject.Controllers
         #endregion Objects and Constructor
 
         #region Documentation
-
         /// <summary>
-        /// Endpoint para listar los miembros.
+        /// Endpoint para listar los miembros. Siendo Usuario o Administrador.
         /// </summary>
-        /// <response code="200">Tarea ejecutada con exito devuelve la lista de miembros.</response>
-        /// <response code="404">No se encontraron datos para mostrar.</response>
-
+        /// <remarks>
+        /// Lista todos los miembros de la BD siendo Administrador. Ejemplo de URL https://api.example.com/v1/Members/
+        /// <br></br>
+        /// Lista los miembros de la BD por página (Id) específicada mediante un query-param enviada por el cliente siendo Usuario. Ejemplo de URL https://api.example.com/v1/Members?page=1
+        /// </remarks>
+        /// <response code="200">Ok. Tarea ejecutada con exito devuelve la lista de miembros.</response>
+        /// <response code="400">BadRequest. Errores de validacion o excepciones.</response> 
+        /// <response code="401">Unauthorized. No se ha indicado o es incorrecto el JWT de acceso.</response> 
+        /// <response code="403">Forbidden. Usted no posee permisos sobre este recurso..</response> 
         #endregion Documentation
 
         [Authorize]
+        [ProducesResponseType(typeof(ResultValue<>), 200)]
+        [ProducesResponseType(typeof(ResultValue<>), 400)]
+        [ProducesResponseType(typeof(Result), 401)]
+        [ProducesResponseType(typeof(ResultValue<>), 403)]
         [HttpGet]
-        public async Task<IActionResult> GetAll([FromQuery]int? page)
+        public async Task<IActionResult> GetAll([FromQuery][Range(1, int.MaxValue, ErrorMessage = "No se admiten números negativos o igual a 0")] int? page)
         {
-            try
-            {
-                if (User.IsInRole("Administrator"))
-                    return Ok(await _memberServices.GetAllAsync());
-                if (!page.HasValue)
-                    return Unauthorized("Usted no esta autorizado.");
 
-                return Ok(await _memberServices.GetAllByPaginationAsync((int)page));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(new Result().Fail(e.Message));
-            }
+            if (User.IsInRole("Administrator"))
+                return Ok(await _memberServices.GetAllAsync());
+            if (!page.HasValue)
+                return StatusCode(403, new ResultValue<IActionResult>() { StatusCode = 403, HasErrors = true, Messages = new List<string>() { "Usted no posee permisos sobre este recurso." } });
+            
+            var result = await _memberServices.GetAllByPaginationAsync((int)page);
+                return StatusCode(result.StatusCode, result);
         }
 
         #region Documentation
-
         /// <summary>
-        /// Endpoint para crear un miembro.
+        /// Endpoint para crear un miembro nuevo.
         /// </summary>
-        /// <response code="200">Tarea ejecutada con exito devuelve un mensaje satisfactorio.</response>
-        /// <response code="400">Errores de validacion o excepciones.</response>
-
+        /// <remarks>
+        /// Crea un nuevo miembro en la BD mediante un formulario. Ejemplo de URL https://api.example.com/v1/Members/
+        /// </remarks>
+        /// <response code="200">Ok. Tarea ejecutada con exito, crea un miembro nuevo.</response>
+        /// <response code="400">BadRequest. Errores de validacion o excepciones.</response> 
+        /// <response code="401">Unauthorized. No se ha indicado o es incorrecto el JWT de acceso.</response> 
         #endregion Documentation
 
         [Authorize]
+        [ProducesResponseType(typeof(Result), 200)]
+        [ProducesResponseType(typeof(Result), 400)]
+        [ProducesResponseType(typeof(Result), 401)]
         [HttpPost]
         public async Task<IActionResult> Create([FromForm] MemberInsertDTO member)
         {
@@ -87,14 +98,20 @@ namespace OngProject.Controllers
         #region Documentation
 
         /// <summary>
-        /// Endpoint para actualizar un miembro.
+        /// Endpoint para actualizar un miembro existente.
         /// </summary>
-        /// <response code="200">Tarea ejecutada con exito devuelve un mensaje satisfactorio.</response>
-        /// <response code="400">Errores de validacion o excepciones.</response>
-
+        /// <remarks>
+        /// Actualiza un miembro existente con el Id proporcionado en la BD mediante un formulario. Ejemplo de URL https://api.example.com/v1/Members/Id
+        /// </remarks>
+        /// <response code="200">Ok. Tarea ejecutada con exito, actualiza un miembro existente.</response>
+        /// <response code="400">BadRequest. Errores de validacion o excepciones.</response> 
+        /// <response code="401">Unauthorized. No se ha indicado o es incorrecto el JWT de acceso.</response> 
         #endregion Documentation
 
         [Authorize]
+        [ProducesResponseType(typeof(Result), 200)]
+        [ProducesResponseType(typeof(Result), 400)]
+        [ProducesResponseType(typeof(Result), 401)]
         [HttpPut("{id}")]
         public async Task<IActionResult> Update([FromForm] MemberUpdateDTO memberUpdate, int id)
         {
