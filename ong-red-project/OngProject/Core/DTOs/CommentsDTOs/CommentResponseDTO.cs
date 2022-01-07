e2e073cacd15ba1630a66940819316e2582e1230
Ricardo <rikigermanramos@gmail.com>
2022-01-07 10:22:27 -0300

    controlador news actualizado con paginacion y nuevo dtoresponse de news

---
+++ ong-red-project/OngProject/Core/DTOs/CommentsDTOs/CommentResponseDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.DTOs.CommentsDTOs
+{
+    public class CommentResponseDTO
+    {
+        public string Body { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
