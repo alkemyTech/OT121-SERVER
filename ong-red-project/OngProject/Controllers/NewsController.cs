7e557612c44665ecea7fc76374d43b84bdea8fbf
Ricardo <rikigermanramos@gmail.com>
2022-01-07 20:48:01 -0300

    endpoints news documentados

--- ong-red-project/OngProject/Controllers/NewsController.cs
+++ ong-red-project/OngProject/Controllers/NewsController.cs
@@ -4,6 +4,7 @@ using OngProject.Common;
 using OngProject.Core.DTOs;
 using OngProject.Core.DTOs.CommentsDTOs;
 using OngProject.Core.DTOs.NewsDTOs;
+using OngProject.Core.Entities;
 using OngProject.Core.Helper.Pagination;
 using OngProject.Core.Interfaces.IServices;
 using System;
@@ -13,6 +14,7 @@ namespace OngProject.Controllers
 {
     [Route("[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class NewsController : ControllerBase
     {
         private readonly INewsServices _newsServices;
@@ -25,12 +27,25 @@ namespace OngProject.Controllers
         /// <summary>
         /// Endpoint para listar los comentarios pertenecientes a un post. 
         /// </summary>
-        /// <response code="200">Listado de todos los comentarios pertenecientes a un post .</response>
-        /// <response code="400">Contenido inválido.</response>
+        /// <remarks>
+        /// Lista por páginas todos los comentarios asociados a un Id de Novedades. Cada página trae a lo sumo 10 items. La página requerida debe ser un número mayor a 0 y menor o igual a la decena del total de cantidad de comentarios de la novedad.
+        ///
+        ///Se requiere credenciales de Usuario o Administrador.
+        ///Ejemplo de solicitud: 
+        ///
+        ///     GET /News/2/comments?page=1
+        ///
+        /// </remarks>
+        /// <response code="200">Petición exitosa.</response>
+        /// <response code="400">Solicitud incorrecta. Id de novedad o número de página inválido.</response>
         /// <response code="401">Credenciales invalidas.</response> 
-        /// <response code="404">No se ha encontrado el dato proporcionado.</response>
+        /// <response code="403">Forbidden. Usted no posee permisos sobre este recurso.</response> 
         #endregion
         [Authorize]
+        [ProducesResponseType(typeof(ResultValue<>), 200)]
+        [ProducesResponseType(typeof(ResultValue<>), 400)]
+        [ProducesResponseType(typeof(ResultValue<>), 401)]
+        [ProducesResponseType(typeof(ResultValue<>), 403)]
         [HttpGet("{id}/comments")]
         public async Task<ActionResult> GetAllCommentsByNews(int id, int page)
         {            
@@ -44,11 +59,25 @@ namespace OngProject.Controllers
 
         #region Documentacion
         /// <summary>
-        /// Endpoint para obtener todos los datos de News por Id
+        /// Endpoint para obtener todos los datos de Novedad.
         /// </summary>
+        /// <remarks>
+        /// Obtiene todos los datos de una Novedad a partir de su Id. 
+        ///Si el Id existe en la base se devuelven sus datos asociados.
+        ///
+        ///No se requieren permisos.
+        ///
+        ///Ejemplo de solicitud: 
+        ///
+        ///     GET /News/1
+        ///
+        /// </remarks>
         /// <response code="200">Solicitud concretada con exito</response>
         /// <response code="404">No encontrado</response> 
         #endregion
+
+        [ProducesResponseType(typeof(News), 200)]
+        [ProducesResponseType(typeof(ActionResult), 404)]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetAsync(int id)
         {
@@ -62,11 +91,23 @@ namespace OngProject.Controllers
         /// <summary>
         /// Endpoint para crear Novedades
         /// </summary>
+        /// <remarks>
+        ///Crea una novedad agregándola a la base de datos. Retorna el Id de la nueva novedad.
+        ///
+        /// Se requieren permisos de Usuario o Administrador
+        ///
+        ///Ejemplo de solicitud: 
+        ///
+        ///     POST /News
+        ///
+        /// </remarks>
         /// <response code="201">Solicitud concretada con exito</response>
         /// <response code="400">Errores de validacion.</response>
         /// <response code="401">Credenciales invalidas</response>  
         #endregion
-
+        [ProducesResponseType(typeof(ActionResult), 201)]
+        [ProducesResponseType(typeof(ActionResult), 400)]
+        [ProducesResponseType(typeof(ActionResult), 401)]
         [HttpPost()]
         [Authorize]
         public async Task<IActionResult> CreateAsync([FromForm] NewsCreateDTO newsDTO)
@@ -79,12 +120,23 @@ namespace OngProject.Controllers
         /// <summary>
         /// Endpoint para actualizar una Novedad.
         /// </summary>
+        /// <remarks>
+        ///Actualiza una novedad en la base de datos. En caso de éxito retorna la novedad actualizada.
+        ///
+        /// Se requieren permisos de Usuario o Administrador
+        ///
+        ///Ejemplo de solicitud: 
+        ///
+        ///     PUT /News/1
+        ///
+        /// </remarks>
         /// <response code="200">Tarea ejecutada con exito devuelve un mensaje satisfactorio.</response>
         /// <response code="400">Errores de validacion o excepciones.</response>
         /// <response code="401">Credenciales invalidas</response>  
-
         #endregion Documentation
-
+        [ProducesResponseType(typeof(News), 200)]
+        [ProducesResponseType(typeof(ActionResult), 400)]
+        [ProducesResponseType(typeof(ActionResult), 401)]
         [HttpPut("{id}")]
         [Authorize]
         public async Task<IActionResult> UpdatePutAsync(int id, [FromForm] NewsUpdateDTO newsUpdateDto)
